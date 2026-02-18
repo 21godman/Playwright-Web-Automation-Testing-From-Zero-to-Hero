45c6f8bf8c60553dd3c1d36962030011ab9058fe
Luka Kuo <jameskuo@21godman-MacBook-Air.local>
2026-02-18 20:40:38 +0800

    Date Picker Page Object

--- tests/usePageObjucts.spec.ts
+++ tests/usePageObjucts.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 import { NavigationPage } from '../page-objucts/navigationPage'
 import { FormLayoutsPage } from '../page-objucts/formLayoutsPage'
+import { DatepickerPage } from '../page-objucts/datepickerPage'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200')
@@ -18,9 +19,13 @@ test('navigate to form page', async ({ page }) => {
 test('parameterized methods', async ({ page }) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatepickerPage = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectedOption('test@example.com', 'Welcome123', 'Option 1')
     await onFormLayoutsPage.submitInlineWithNameEmailAndCheckbox('John Doe', 'Luka.kuo@example.com', true)
-    await page.pause()
+
+    await navigateTo.datepickerPage()
+    await onDatepickerPage.selectCommonDatepickerDateFromToday(90)
+    await onDatepickerPage.selectDatepickerWithRangeFromToday(10, 20)
 })
